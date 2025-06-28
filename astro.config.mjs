83e7cd0d0c35356429149a344e03444cf832e930
jeissonmgz <jeissonmgz@gmail.com>
2025-06-27 21:37:13 -0500

    :mag: Add robots and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,29 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-	i18n: {
-		locales: ["es", "en"],
-		defaultLocale: "en",
-		routing: {
-			prefixDefaultLocale: false
-		}
+  i18n: {
+      locales: ["es", "en"],
+      defaultLocale: "en",
+      routing: {
+          prefixDefaultLocale: false
+      }
 	},
-	vite: {
-		plugins: [tailwindcss()],
+
+  vite: {
+      plugins: [tailwindcss()],
 	},
+  site: 'https://image.isreadyfor.work',
+  integrations: [sitemap({
+      i18n: {
+        defaultLocale: 'en',
+        locales: {
+          en: 'en',
+          es: 'es',
+        },
+      },
+    })],
 });
